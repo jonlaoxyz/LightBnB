57326c4118994b12b0228b288920abc7515a8f1a
Jonathan Lao <jon.lao@gmail.com>
2023-12-14 20:37:10 -0600

    refactor getAllProperties - add other functionalities from search

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -148,18 +148,71 @@ exports.getAllReservations = getAllReservations;
 }; */
 
 
-const getAllProperties = (options, limit = 10) => {
+const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if(options.owner_id || options.minimum_price_per_night && options.maximum_price_per_night){
+    queryString += `WHERE`
+  }
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    if(options.city) {
+      queryString +=  ` AND `
+    }
+    queryParams.push(`${options.owner_id}`);
+    queryString +=  ` owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    if(options.city || options.owner_id){
+      queryString +=  ` AND `
+    }
+    let minPrice = options.minimum_price_per_night * 100
+    let maxPrice = options.maximum_price_per_night * 100
+
+    queryParams.push(`${minPrice}`);
+    queryParams.push(`${maxPrice}`);
+
+    queryString += ` (properties.cost_per_night > $${queryParams.length-1} AND properties.cost_per_night < $${queryParams.length}) `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+  //return pool.query(queryString, queryParams).then((res) => res.rows);
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(
+      queryString,
+      queryParams)
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
-    })
+      })
     .catch((err) => {
       console.log(err.message);
-    });
+      });
 };
-
+exports.getAllProperties = getAllProperties;
 
 /**
  * Add a property to the database
